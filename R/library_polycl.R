59b5b025d79a3ff34bfcb09b43f9a55b7aef70cb
brandonsie <brandonsie@gmail.com>
2019-03-13 22:26:04 -0400

    0.1.7 bugfix

--- R/library_polycl.R
+++ R/library_polycl.R
@@ -10,20 +10,37 @@
 #' @export
 
 library_polycl <- function(data, annot, pairs){
+  print("data dim:")
+  print(dim(data))
+
+  print("annot dim:")
+  print(dim(annot))
+
+  print("pairs dim:")
+  print(dim(pairs))
+
+
   pro_ids <- annot$pro_id[match(data[,1], annot$u_pep_id)]  %>% na.omit
   proteins <- annot$pro_id %>% unique
+
+  print(paste("length pro ids", length(pro_ids)))
+  print(paste("length proteins:", length(proteins)))
 
   #setup polyclonal table
   lib_polycl <- data.frame(matrix(nrow = length(proteins), ncol = ncol(data)))
   colnames(lib_polycl) <- c("pro_id", colnames(data)[-1])
   lib_polycl[,1] <- proteins
 
+  print("dim lib polycl:")
+  print(dim(lib_polycl))
+
   #loop through this library's proteins
   pb <- pbar(0, length(proteins))
   for(i in 1:length(proteins)){
+    print(i)
     if(interactive()){setTxtProgressBar(pb, i)}
     lib_polycl[i, -1] <- data[c(1:length(pro_ids))[
-      pro_ids == proteins[i]], -1] %>% na.omit %>% protein_polycl(pairs)
+      pro_ids == proteins[i]], ] %>% na.omit %>% protein_polycl(pairs) #in bracket , used to be ...  == proteins[i]], -1]. remove d-1 to feed to protein polycl
   }
 
   return(lib_polycl)
--- R/read_pairs.R
+++ R/read_pairs.R
@@ -10,21 +10,21 @@
 
 read_pairs <- function(libname, pairs_path){
   #prepare to load intrapeptide alignment pairs for specific library
-  print(paste("libname:", libname))
-  print(paste("pairs path:", pairs_path))
+  # print(paste("libname:", libname))
+  # print(paste("pairs path:", pairs_path))
   pairs_files <- list.files(pairs_path)
 
   # specify annotation file path
   lib_pairs_name <- pairs_files[grep(libname,pairs_files)] %>% min
-  print(paste("lib_pairs_name", lib_pairs_name))
+  # print(paste("lib_pairs_name", lib_pairs_name))
   lib_pairs_path <- paste(pairs_path, lib_pairs_name, sep = "/")
-  print(paste("lib pairs path", lib_pairs_path))
+  # print(paste("lib pairs path", lib_pairs_path))
 
   lib_pairs_files <- list.files(lib_pairs_path)
-  print(paste("lib pairs files", lib_pairs_files))
+  # print(paste("lib pairs files", lib_pairs_files))
   lib_pairs_names <- lib_pairs_files[intersect(
     grep(libname,lib_pairs_files), grep("_intra", lib_pairs_files))]
-  print(paste("lib pairs name", lib_pairs_names))
+  # print(paste("lib pairs name", lib_pairs_names))
 
   lib_pairs_file_path <- paste(lib_pairs_path, lib_pairs_names, sep = "/")
   print(paste("lib pairs file path", lib_pairs_file_path))
