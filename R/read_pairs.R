75445479a0cd5e164e025c3e71ca86218b87afa9
brandonsie <brandonsie@gmail.com>
2019-05-06 17:01:38 -0400

    reorganized some source files

--- R/read_pairs.R
+++ R/read_pairs.R
@@ -1,3 +1,29 @@
+#' Read blast alignment pair files based on library names and metadata path.
+#'
+#' @param libs Character vector of library names.
+#' @param pairs_path Metadata directory as character string.
+#'
+#' @return phiplist of blast pairs First element is character vector of
+#' libraries from libs input. Remaining elements are corresponding alignment
+#' files.
+#'
+#' @export
+
+
+
+read_pairs_list <- function(libs, pairs_path){
+  # prep output data list
+  output_data <- list()
+  output_data[[1]] <- libs
+
+  for(i in 1:length(libs)){
+    output_data[[i+1]] <- read_pairs(libs[i], pairs_path)
+  }
+
+  return(output_data)
+
+}
+
 #' Read BLAST pairs file based on md path and library name.
 #'
 #' @param libname Library name.
