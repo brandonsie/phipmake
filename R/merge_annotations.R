846e05fa700acbc7f590a0206854183783c696a4
brandonsie <brandonsie@gmail.com>
2019-08-09 11:25:26 -0400

    bugfix epf

--- R/merge_annotations.R
+++ R/merge_annotations.R
@@ -9,9 +9,14 @@ merge_annotations <- function(enrichment_annotations){
 
   a <- lapply(enrichment_annotations, function(x)
     x[, c("u_pep_id", "gene_symbol", "pep_rank", "taxon_species", "pep_aa")])
-  a$annotation <- paste(a$gene_symbol, a$pep_rank, a$taxon_species, sep = "_")
 
-  a <- dplyr::bind_rows(a)
+  a <- lapply(a, as.matrix)
+  a <- lapply(a, as.data.frame)
+
+  a <- dplyr::bind_rows(a,)
+
+
+  a$annotation <- paste(a$gene_symbol, a$pep_rank, a$taxon_species, sep = "_")
 
 
   return(a)
